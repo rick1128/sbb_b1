9c3ec00d153c7e9c03ac543dbe299b9a377c6af5
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-17 09:02:29 +0300

    Update الكشف.py

--- sbb_b/plugins/الكشف.py
+++ sbb_b/plugins/الكشف.py
@@ -1,46 +1,75 @@
 import html
 import os
+import base64
+
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import MessageEntityMentionName
 
 from requests import get
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
-from telethon.utils import get_input_location
 
 from sbb_b import sbb_b
 from sbb_b.core.logger import logging
 
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers import get_user_from_event, reply_id
-from . import spamwatch
+from sbb_b.Config import Config
+from sbb_b.core.managers import edit_or_reply, edit_delete
+from sbb_b.helpers import reply_id
+from sbb_b.sql_helper.globals import gvarstatus
+from sbb_b.plugins import spamwatch
 
 LOGS = logging.getLogger(__name__)
 
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
 
 async def fetch_info(replied_user, event):
+    """Get details from the User object."""
+    FullUser = (await event.client(GetFullUserRequest(replied_user.id))).full_user
     replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(
-            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
-        )
+        GetUserPhotosRequest(user_id=replied_user.id, offset=42, max_id=0, limit=80)
     )
     replied_user_profile_photos_count = "⌔∮ هذا المستخدم لم يضع اي صورة"
+    dc_id = "Can't get dc id"
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
+        dc_id = replied_user.photo.dc_id
     except AttributeError:
         pass
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    last_name = replied_user.user.last_name
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        pass
-    common_chat = replied_user.common_chats_count
-    username = replied_user.user.username
-    user_bio = replied_user.about
-    replied_user.user.bot
-    replied_user.user.restricted
-    replied_user.user.verified
+    user_id = replied_user.id
+    first_name = replied_user.first_name
+    full_name = FullUser.private_forward_name
+    common_chat = FullUser.common_chats_count
+    username = replied_user.username
+    user_bio = FullUser.about
+    is_bot = replied_user.bot
+    restricted = replied_user.restricted
+    verified = replied_user.verified
     photo = await event.client.download_profile_photo(
         user_id,
         Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",
@@ -49,10 +78,10 @@ async def fetch_info(replied_user, event):
     first_name = (
         first_name.replace("\u2060", "")
         if first_name
-        else ("⌔∮ هذا المستخدم ليس لديه اسم اول")
+        else ("هذا المستخدم ليس لديه اسم اول")
     )
-    last_name = last_name.replace("\u2060", "") if last_name else (" ")
-    username = "@{}".format(username) if username else ("⌔∮ هذا الشخص ليس لديه معرف ")
+    full_name = full_name or first_name
+    username = "@{}".format(username) if username else ("⌔∮ هذا المستخدم ليس لديه معرف")
     user_bio = "⌔∮ هذا المستخدم ليس لديه اي نبذة" if not user_bio else user_bio
     rozrtba = (
         ".「  مطـور السورس  」."
@@ -66,99 +95,32 @@ async def fetch_info(replied_user, event):
         and user_id != 2034443585
         and user_id != 1280124974
         else rozrtba
-    )
+    )    
     caption = " \n"
-    caption += f"╽<b>- الاسـم ⇜</b> {first_name} {last_name}\n"
-    caption += f"╽<b>- المـعـرف ⇜</b> {username}\n"
+    caption += f"╽<b>- الاسـم ⇜ </b> {full_name}\n"
+    caption += f"╽<b>- المـعـرف ⇜ </b> {username}\n"
     caption += f"╽<b>- الايـدي  ⇜</b> <code>{user_id}</code>\n"
-    caption += f"╽<b>- عـدد الصـورة ⇜</b> {replied_user_profile_photos_count}\n"
     caption += f"╽<b>- الـمجموعات المشتـركة ⇜</b> {common_chat}\n"
+    caption += f"╽<b>- عـدد الصـورة ⇜</b> {replied_user_profile_photos_count}\n"
     caption += f"╽<b>- الرتبـة ⇜</b>{rozrtba}\n"
     caption += f"╽<b>-️ الـنبـذه ⇜</b> \n<code>{user_bio}</code>\n\n"
     caption += f"╽<b>- رابط حسـابه ⇜</b> "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>\n'
     return photo, caption
 
-
-@sbb_b.ar_cmd(pattern="كشف(?:\s|$)([\s\S]*)")
-async def _(event):
-    replied_user, error_i_a = await get_user_from_event(event)
-    if not replied_user:
-        return
-    rozevent = await edit_or_reply(
-        event, "⌔∮ جار إحضار معلومات المستخدم اننظر قليلا ⚒️"
-    )
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
-    user_id = replied_user.user.id
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
-    common_chats = replied_user.common_chats_count
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception:
-        dc_id = "- لم يتم التعرف بشكل صحيح"
-    if spamwatch:
-        ban = spamwatch.get_ban(user_id)
-        if ban:
-            sw = f"**حاله قيود الحظر :** `محظور` \n       **-**🤷‍♂️**السبب : **`{ban.reason}`"
-        else:
-            sw = f"**حاله قيود الحظر :** `غير محظور`"
-    else:
-        sw = "**حاله قيود الحظر  :**`لم يتم التعرف`"
-    try:
-        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
-        data = get(casurl).json()
-    except Exception as e:
-        LOGS.info(e)
-        data = None
-    if data:
-        if data["ok"]:
-            cas = "**Antispam(CAS) Banned :** `True`"
-        else:
-            cas = "**Antispam(CAS) Banned :** `False`"
-    else:
-        cas = "**Antispam(CAS) Banned :** `Couldn't Fetch`"
-    caption = """**معلومات المسـتخدم[{}](tg://user?id={}):
-   ⪼ ⚕️ الايدي: **`{}`
-   ⪼ 👥**المجموعات المشتركه : **`{}`
-   ⪼ 🌏**رقم قاعده البيانات : **`{}`
-   ⪼ 🔏**هل هو حساب موثق  : **`{}`
-""".format(
-        first_name,
-        user_id,
-        user_id,
-        common_chats,
-        dc_id,
-        replied_user.user.restricted,
-        sw,
-        cas,
-    )
-    await edit_or_reply(rozevent, caption)
-
-
-@sbb_b.ar_cmd(pattern="ايدي(?:\s|$)([\s\S]*)")
+@sbb_b.ar_cmd(pattern="ايدي(?: |$)(.*)")
 async def who(event):
+    roz = await edit_or_reply(event, "**⌔∮ جار التعرف على المستخدم انتظر قليلا**")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    replied_user, reason = await get_user_from_event(event)
-    if not replied_user:
-        return
-    roz = await edit_or_reply(event, "**⌔∮ يتم استخراج معلومات المستخدم **")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
+    replied_user = await get_user_from_event(event)
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return await edit_or_reply(
-            roz, "**⌔∮ لم يتم العثور على معلومات لهذا المستخدم **"
-        )
-    message_id_to_reply = await reply_id(event)
+        return await edit_or_reply(roz,  "**⌔∮ لم يتم العثور على معلومات لهذا المستخدم **")
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
     try:
         await event.client.send_file(
             event.chat_id,

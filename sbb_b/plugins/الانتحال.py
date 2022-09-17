338b4c73a230d0c8b57c894ad4eade77b030af0d
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-17 10:16:59 +0300

    Update الانتحال.py

--- sbb_b/plugins/الانتحال.py
+++ sbb_b/plugins/الانتحال.py
@@ -2,123 +2,67 @@ import html
 
 from telethon.tl import functions
 from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
 
-from . import *
+from sbb_b.Config import Config
+from sbb_b.sql_helper.globals import gvarstatus
+from sbb_b.plugins import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, sbb_b, edit_delete, get_user_from_event
 
 DEFAULTUSER = gvarstatus("FIRST_NAME") or ALIVE_NAME
-DEFAULTUSERBIO = gvarstatus("DEFAULT_BIO") or "﴿ لا تَحزَن إِنَّ اللَّهَ مَعَنا ﴾"
-
+DEFAULTUSERBIO = Config.DEFAULT_BIO or "﴿ لا تَحزَن إِنَّ اللَّهَ مَعَنا ﴾"
+ANT7AL = gvarstatus("ANT7AL") or "(اعادة الحساب|اعادة)"
 
 @sbb_b.ar_cmd(pattern="انتحال(?:\s|$)([\s\S]*)")
 async def _(event):
-    reply_message = await event.get_reply_message()
-    replied_user, error_i_a = await get_full_user(event)
+    replied_user, error_i_a = await get_user_from_event(event)
     if replied_user is None:
-        await edit_delete(event, str(error_i_a))
-        return False
-    user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(user_id, Config.TEMP_DIR)
-    first_name = html.escape(replied_user.user.first_name)
-    if user_id == 1280124974:
-        await event.edit("⌔∮ ههه لا يمكنك انتحال مطور السورس العب بعيد عمو")
-        await asyncio.sleep(3)
         return
+    user_id = replied_user.id
+    profile_pic = await event.client.download_profile_photo(user_id, Config.TEMP_DIR)
+    first_name = html.escape(replied_user.first_name)
     if first_name is not None:
         first_name = first_name.replace("\u2060", "")
-    last_name = replied_user.user.last_name
+    last_name = replied_user.last_name
     if last_name is not None:
         last_name = html.escape(last_name)
         last_name = last_name.replace("\u2060", "")
     if last_name is None:
-        last_name = "⁪⁬⁮⁮⁮⁮ ‌"
+        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+    replied_user = (await event.client(GetFullUserRequest(replied_user.id))).full_user
     user_bio = replied_user.about
     if user_bio is not None:
         user_bio = replied_user.about
     await event.client(functions.account.UpdateProfileRequest(first_name=first_name))
     await event.client(functions.account.UpdateProfileRequest(last_name=last_name))
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
-    pfile = await event.client.upload_file(profile_pic)
+    try:
+        pfile = await event.client.upload_file(profile_pic)
+    except Exception as e:
+        return await edit_delete(event, f"**فشل في الانتحال بسبب:**\n__{e}__")
     await event.client(functions.photos.UploadProfilePhotoRequest(pfile))
-    await event.delete()
-    await event.client.send_message(
-        event.chat_id, "**- تم بنجاح انتحال المستخدم**", reply_to=reply_message
-    )
-    await event.client.send_message(
-        BOTLOG_CHATID,
-        f"#الانتحال \n\n**تم بنجاح انتحال**  [{first_name}](tg://user?id={user_id })",
-    )
+    await edit_delete(event, "**- تم بنجاح انتحال حساب المستخدم**")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#الانتحال\nتم انتحال المستخدم: [{first_name}](tg://user?id={user_id })",
+        )
 
 
-@sbb_b.ar_cmd(pattern="اعادة الحساب$")
-async def _(event):
+@sbb_b.ar_cmd(pattern=f"{ANT7AL}$")
+async def revert(event):
     firstname = DEFAULTUSER
     lastname = gvarstatus("LAST_NAME") or ""
     bio = DEFAULTUSERBIO
-    roz = 1
     await event.client(
         functions.photos.DeletePhotosRequest(
-            await event.client.get_profile_photos("me", limit=roz)
+            await event.client.get_profile_photos("me", limit=1)
         )
     )
     await event.client(functions.account.UpdateProfileRequest(about=bio))
     await event.client(functions.account.UpdateProfileRequest(first_name=firstname))
     await event.client(functions.account.UpdateProfileRequest(last_name=lastname))
-    await edit_or_reply(event, "**- تم بنجاح ارجاع الحساب الى وضعه الاصلي**")
-    await event.client.send_message(
-        BOTLOG_CHATID, f"- تم اعادة الحساب الى وضعه الاصلي ✓"
-    )
-
-
-async def get_full_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.forward.sender_id
-                    or previous_message.forward.channel_id
-                )
-            )
-            return replied_user, None
-        else:
-            replied_user = await event.client(
-                GetFullUserRequest(previous_message.sender_id)
-            )
-            return replied_user, None
-    else:
-        input_str = None
-        try:
-            input_str = event.pattern_match.group(1)
-        except IndexError as e:
-            return None, e
-        if event.message.entities:
-            mention_entity = event.message.entities
-            probable_user_mention_entity = mention_entity[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            else:
-                try:
-                    user_object = await event.client.get_entity(input_str)
-                    user_id = user_object.id
-                    replied_user = await event.client(GetFullUserRequest(user_id))
-                    return replied_user, None
-                except Exception as e:
-                    return None, e
-        elif event.is_private:
-            try:
-                user_id = event.chat_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        else:
-            try:
-                user_object = await event.client.get_entity(int(input_str))
-                user_id = user_object.id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
+    await edit_delete(event, "**- تم بنجاح ارجاع الحساب الى وضعه الاصلي**")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "#الاعادة\nتم بنجاح اعادة الحساب الى وضعه السابق",
+        )

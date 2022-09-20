0b7e3127f21e336285373fe647d88b2b9c4cc969
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-20 14:12:13 +0300

    Update التحديث.py

--- sbb_b/plugins/التحديث.py
+++ sbb_b/plugins/التحديث.py
@@ -250,7 +250,7 @@ async def upstream(event):
             f"⌔∮ يبدو انك نصبت جمقون عبر السيرفر يرجى ارسال `{cmdhd}تحديث الان`",
         )
     event = await edit_or_reply(event, "⌔∮ يتم الان الوصول الى معلومات التحديث انتظر")
-    off_repo = "https://github.com/sbb-b/zuhairy0"
+    off_repo = "https://github.com/jmthonar/zuhairy"
     os.chdir("/app")
     try:
         txt = "**⌔∮ عذرا لم يتم اكمال التحديث بسبب بعض الاخطاء " + "**اللوگ:**\n"

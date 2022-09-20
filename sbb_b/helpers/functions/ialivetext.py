e632be6f791d2e23281197a332f0be9d89623a42
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-20 20:23:24 +0300

    Update ialivetext.py

--- sbb_b/helpers/functions/ialivetext.py
+++ sbb_b/helpers/functions/ialivetext.py
@@ -82,7 +82,7 @@ async def jmthonalive(StartTime):
         dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
     except Exception as e:
         dyno = e
-    return f"🖤༄ سورس جمثون ༄🖤\
+    return f"🖤༄ سورس ريك ثون ༄🖤\
                  \n\nღ ثاعدة البيانات : {check_sgnirts}\
                   \nღ المطور الثانوي : {sudo}\
                   \nღ الوقت : {uptime}\

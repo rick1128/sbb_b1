06428fa7f9b9f2b53dc8707350d7a1edd6872724
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-22 18:01:41 +0300

    Update startup.py

--- sbb_b/utils/startup.py
+++ sbb_b/utils/startup.py
@@ -81,10 +81,6 @@ async def saves():
         await sbb_b(JoinChannelRequest("@RR7PP"))
     except BaseException:
         pass
-    try:
-        await sbb_b(JoinChannelRequest("@haa9k"))
-    except BaseException:
-        pass
 
 
 async def mybot():

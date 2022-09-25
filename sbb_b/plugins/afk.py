7ded595c7e6b1d4d4150a72c632426b2bce581d1
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-26 01:37:28 +0300

    Update afk.py

--- sbb_b/plugins/afk.py
+++ sbb_b/plugins/afk.py
@@ -11,6 +11,7 @@ from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.tools import media_type
 from ..helpers.utils import _format
 from . import BOTLOG, BOTLOG_CHATID
+from ..sql_helper.globals import gvarstatus
 
 LOGS = logging.getLogger(__name__)
 

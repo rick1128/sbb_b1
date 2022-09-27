84d663a8794596c6ecb9b6397f7a0ec8c44d0434
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-27 23:38:25 +0300

    Update __init__.py

--- sbb_b/__init__.py
+++ sbb_b/__init__.py
@@ -14,13 +14,13 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.2.0"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "سورس جمثون <https://github.com/sbb-b/sbb_b>"
-__copyright__ = f" حقوق سورس جمثون (C) 2020 - 2022  {__author__}"
+__author__ = "سورس ريك ثون <https://github.com/sbb-b/sbb_b>"
+__copyright__ = f" حقوق سورس ريك ثون (C) 2020 - 2022  {__author__}"
 
 sbb_b.version = __version__
 sbb_b.tgbot.version = __version__
-LOGS = logging.getLogger("سورس جمثون")
-bot = sbb_b
+LOGS = logging.getLogger("سورس ريك ثون")
+bot = sbb_b0
 
 StartTime = time.time()
 jmthonversion = "5.1.1"

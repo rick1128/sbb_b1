5f2320fd384e541c9d758ebb8f1c45c161dbe4ea
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-28 19:32:55 +0300

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -5,14 +5,13 @@ from datetime import datetime
 from platform import python_version
 
 import requests
-from telethon import version
+from telethon import version, Button, events
 from telethon.errors.rpcerrorlist import (
     MediaEmptyError,
     WebpageCurlFailedError,
     WebpageMediaEmptyError,
 )
 from telethon.events import CallbackQuery
-
 from sbb_b import StartTime, jmthonversion, sbb_b
 
 from ..Config import Config
@@ -91,15 +90,64 @@ def jmthonalive_text():
     return jmthon_caption
 
 
+
 @sbb_b.ar_cmd(pattern="السورس$")
-async def amireallyalive(event):
-    reply_to_id = await reply_id(event)
-    results = await event.client.inline_query(Config.TG_BOT_USERNAME, "ألسورس")
-    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+async def repo(event):
+    RR7PP = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await sbb_b.inline_query(RR7PP, "السورس")
+    await response[0].click(event.chat_id)
     await event.delete()
 
+ROZ_PIC = "https://telegra.ph/file/5f6ef13851dcf0d6fc72b.jpg"
+RAZAN = Config.TG_BOT_USERNAME
+ROZ_T = (
+    f"**⌯︙بوت جمثـون يعمل بنجاح 🤍،**\n"
+    f"**   - اصدار التليثون :** `1.23.0\n`"
+    f"**   - اصدار جمثون :** `4.0.0`\n"
+    f"**   - البوت المستخدم :** `{RAZAN}`\n"
+    f"**   - اصدار البايثون :** `3.9.6\n`"
+    f"**   - المستخدم :** {mention}\n"
+)
 
-@sbb_b.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
-async def on_plug_in_callback_query_handler(event):
-    statstext = await jmthonalive(StartTime)
-    await event.answer(statstext, cache_time=0, alert=True)
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        await sbb_b.get_me()
+        if query.startswith("السورس") and event.query.user_id == sbb_b.uid:
+            buttons = [
+                [
+                    Button.url("قنـاة السـورس ⚙️", "https://t.me/JMTHON"),
+                    Button.url("المطـور 👨🏼‍💻", "https://t.me/RR77R"),
+                ]
+            ]
+            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    ROZ_PIC, text=ROZ_T, buttons=buttons, link_preview=False
+                )
+            elif ROZ_PIC:
+                result = builder.document(
+                    ROZ_PIC,
+                    title="JMTHON - USERBOT",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="JMTHON - USERBOT",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            await event.answer([result] if result else None)
+
+
+
+
+# edit by ~ @RR77R

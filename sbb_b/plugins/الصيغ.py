bf5d4eeea8f6a0d3fba7cbd0480ea243b0d09b5a
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-30 01:51:28 +0300

    Update الصيغ.py

--- sbb_b/plugins/الصيغ.py
+++ sbb_b/plugins/الصيغ.py
@@ -364,53 +364,41 @@ async def _(event):
 @sbb_b.ar_cmd(pattern="تحويل الى (mp3|voice)")
 async def _(event):
     if not event.reply_to_msg_id:
-        return await edit_or_reply(
-            event, "**- يجب عليك الرد على الميديا المراد تحويلها**"
-        )
+        await edit_or_reply(event, "**- يجب عليك الرد على الميديا المراد تحويلها**")
+        return
     reply_message = await event.get_reply_message()
     if not reply_message.media:
-        return await edit_or_reply(
-            event, "**- يجب عليك الرد على الميديا المراد تحويلها**"
-        )
+        await edit_or_reply(event, "**- يجب عليك الرد على الميديا المراد تحويلها**")
+        return
     input_str = event.pattern_match.group(1)
-    if input_str is None:
-        return await edit_delete(
-            event, f"- يجب عليك الرد على الميديا ب `.تحويل صوتي` او `.تحويل بصمة`"
-        )
-    if input_str in ["mp3", "voice"]:
-        roz = await edit_delete(event, "**- جار التحويل انتظر قليلا ...**")
-    else:
-        return await edit_delete(
-            event, f"- يجب عليك الرد على الميديا ب `.تحويل صوتي` او `.تحويل بصمة`"
-        )
+    event = await edit_or_reply(event, "**- جار التحويل انتظر قليلا ...**")
     try:
-        start = datetime.datetime.now()
+        start = datetime.now()
         c_time = time.time()
         downloaded_file_name = await event.client.download_media(
             reply_message,
             Config.TMP_DOWNLOAD_DIRECTORY,
             progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, roz, c_time, "- جار التحميل انتظر قليلا")
+                progress(d, t, event, c_time, "trying to download")
             ),
         )
     except Exception as e:
         await event.edit(str(e))
     else:
-        end = datetime.datetime.now()
+        end = datetime.now()
         ms = (end - start).seconds
-        await roz.edit(
-            f"- تم تنزيل الملف : {downloaded_file_name}\nالوقت المستغرق: {ms} من الثواني"
-        )
+        await event.edit(f"- تم تنزيل الملف : {downloaded_file_name}\nالوقت المستغرق: {ms} من الثواني")
         new_required_file_name = ""
         new_required_file_caption = ""
         command_to_run = []
         voice_note = False
         supports_streaming = False
         if input_str == "voice":
-            new_required_file_caption = "voice_" + str(round(time.time())) + ".opus"
+            new_required_file_caption = f"voice_{str(round(time.time()))}.opus"
             new_required_file_name = (
-                Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
+                f"{Config.TMP_DOWNLOAD_DIRECTORY}/{new_required_file_caption}"
             )
+
             command_to_run = [
                 "ffmpeg",
                 "-i",
@@ -428,10 +416,11 @@ async def _(event):
             voice_note = True
             supports_streaming = True
         elif input_str == "mp3":
-            new_required_file_caption = "mp3_" + str(round(time.time())) + ".mp3"
+            new_required_file_caption = f"mp3_{str(round(time.time()))}.mp3"
             new_required_file_name = (
-                Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
+                f"{Config.TMP_DOWNLOAD_DIRECTORY}/{new_required_file_caption}"
             )
+
             command_to_run = [
                 "ffmpeg",
                 "-i",
@@ -442,7 +431,7 @@ async def _(event):
             voice_note = False
             supports_streaming = True
         else:
-            await edit_or_reply(roz, "**- عليك استخدام الامر بشكل صحيح**")
+            await event.edit("**- هذه الصيغة غير مدعومة**")
             os.remove(downloaded_file_name)
             return
         process = await asyncio.create_subprocess_exec(
@@ -465,8 +454,9 @@ async def _(event):
                 voice_note=voice_note,
                 supports_streaming=supports_streaming,
                 progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, roz, c_time, "**- جار الرفع انتظر قليلا**")
+                    progress(d, t, event, c_time, "trying to upload")
                 ),
             )
             os.remove(new_required_file_name)
             await event.delete()
+

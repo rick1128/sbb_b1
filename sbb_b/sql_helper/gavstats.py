26be976a6fa91fab412ffd23be827da99f814d37
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-30 23:33:22 +0300

    Create gavstats.py

---
+++ sbb_b/sql_helper/gavstats.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, String, delete
+from sqlalchemy.orm.exc import NoResultFound
+
+from . import BASE, SESSION
+
+
+class grism(BASE):
+    __tablename__ = "autogrpe"
+    agroup = Column(String, primary_key=True)
+
+    def __init__(self, agroup):
+        self.agroup = str(agroup)
+
+
+grism.__table__.create(checkfirst=True)
+
+
+def autogroup(
+    agroup,
+):
+    try:
+        if SESSION.query(grism).one():
+            del_autogroup()
+    except NoResultFound:
+        pass
+    user = grism(agroup)
+    SESSION.add(user)
+    SESSION.commit()
+    SESSION.close()
+    return True
+
+
+def del_autogroup():
+    to_check = get_autogroup()
+    if not to_check:
+        return False
+    stmt = delete(grism)
+    SESSION.execute(stmt)
+    SESSION.commit()
+    # SESSION.close()
+    return stmt
+
+
+def get_autogroup():
+    try:
+        if _result := SESSION.query(grism).one().agroup:
+            return _result
+    except NoResultFound:
+        return None
+    finally:
+        SESSION.close()

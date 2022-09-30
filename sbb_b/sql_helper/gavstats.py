1e83337308a005afda2e87b4d5ac6c15c87008f2
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-30 22:13:12 +0300

    Update gavstats.py

--- sbb_b/sql_helper/gavstats.py
+++ sbb_b/sql_helper/gavstats.py
@@ -1,34 +1,31 @@
 from sqlalchemy import Column, String, delete
 from sqlalchemy.orm.exc import NoResultFound
-
 from . import BASE, SESSION
-
 
 class gavestaus(BASE):
     __tablename__ = "gavstats"
-    value = Column(String, primary_key=True)
+    jasem = Column(String, primary_key=True)
 
-    def __init__(self, value):
-        self.value = str(value)
+    def __init__(self, jasem):
+        self.jasem = str(jasem)
 
 
 gavestaus.__table__.create(checkfirst=True)
 
 
-def addvar(value):
+def addvar(jasem):
     try:
         if SESSION.query(gavestaus).one():
             del_addvar()
     except NoResultFound:
         pass
-    user = gavestaus(value)
+    user = gavestaus(jasem)
     SESSION.add(user)
     SESSION.commit()
     SESSION.close()
     return True
 
-
-def del_addvar():
+def del_addvar():   
     to_check = get_addvar()
     if not to_check:
         return False
@@ -37,10 +34,9 @@ def del_addvar():
     SESSION.commit()
     return adder
 
-
 def get_addvar():
     try:
-        if _result := SESSION.query(gavestaus).one().value:
+        if _result := SESSION.query(gavestaus).one().jasem:
             return _result
     except NoResultFound:
         return None

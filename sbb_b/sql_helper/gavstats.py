343eab971d818bf61c07dc88c8c713291e081d47
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-30 22:34:47 +0300

    Create gavstats.py

--- sbb_b/sql_helper/gavstats.py
+++ sbb_b/sql_helper/gavstats.py
@@ -1,46 +1,45 @@
 from sqlalchemy import Column, String, delete
 from sqlalchemy.orm.exc import NoResultFound
-
 from . import BASE, SESSION
 
+class grism(BASE):
+    __tablename__ = "autogrpe"
+    agroup = Column(String, primary_key=True)
 
-class gavestaus(BASE):
-    __tablename__ = "gavstats"
-    groupez = Column(String, primary_key=True)
-
-    def __init__(self, groupez):
-        self.groupez = str(groupez)
-
-
-gavestaus.__table__.create(checkfirst=True)
+    def __init__(self, agroup):
+        self.agroup = str(agroup)
 
 
-def addvar(groupez):
+grism.__table__.create(checkfirst=True)
+
+
+def autogroup(
+    agroup,
+):
     try:
-        if SESSION.query(gavestaus).one():
-            del_addvar()
+        if SESSION.query(grism).one():
+            del_autogroup()
     except NoResultFound:
         pass
-    user = gavestaus(groupez)
+    user = grism(agroup)
     SESSION.add(user)
     SESSION.commit()
     SESSION.close()
     return True
 
-
-def del_addvar():
-    to_check = get_addvar()
+def del_autogroup():   
+    to_check = get_autogroup()
     if not to_check:
         return False
-    abne = delete(gavestaus)
-    SESSION.execute(abne)
+    stmt = delete(grism)
+    SESSION.execute(stmt)
     SESSION.commit()
-    return abne
+    #SESSION.close()
+    return stmt
 
-
-def get_addvar():
+def get_autogroup():
     try:
-        if _result := SESSION.query(gavestaus).one().groupez:
+        if _result := SESSION.query(grism).one().agroup:
             return _result
     except NoResultFound:
         return None

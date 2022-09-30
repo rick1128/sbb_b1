19f52fbdf664213569d945b69959908362e822ac
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-30 21:48:16 +0300

    Create gavstats.py

---
+++ sbb_b/sql_helper/gavstats.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, String, delete
+from sqlalchemy.orm.exc import NoResultFound
+from . import BASE, SESSION
+
+class gavestaus(BASE):
+    __tablename__ = "gavstats"
+    value = Column(String, primary_key=True)
+
+    def __init__(self, value):
+        self.value = str(value)
+
+
+gavestaus.__table__.create(checkfirst=True)
+
+
+def addvar(value):
+    try:
+        if SESSION.query(gavestaus).one():
+            del_addvar()
+    except NoResultFound:
+        pass
+    user = gavestaus(value)
+    SESSION.add(user)
+    SESSION.commit()
+    SESSION.close()
+    return True
+
+def del_addvar():   
+    to_check = get_addvar()
+    if not to_check:
+        return False
+    adder = delete(gavestaus)
+    SESSION.execute(adder)
+    SESSION.commit()
+    return adder
+
+def get_addvar():
+    try:
+        if _result := SESSION.query(gavestaus).one().value:
+            return _result
+    except NoResultFound:
+        return None
+    finally:
+        SESSION.close()

c17aa23fb548ece1f28c767b0fc2d38f4e98a9a5
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-09-30 22:20:41 +0300

    Update gavstats.py

--- sbb_b/sql_helper/gavstats.py
+++ sbb_b/sql_helper/gavstats.py
@@ -1,46 +1,42 @@
 from sqlalchemy import Column, String, delete
 from sqlalchemy.orm.exc import NoResultFound
-
 from . import BASE, SESSION
-
 
 class gavestaus(BASE):
     __tablename__ = "gavstats"
-    jasem = Column(String, primary_key=True)
+    groupez = Column(String, primary_key=True)
 
-    def __init__(self, jasem):
-        self.jasem = str(jasem)
+    def __init__(self, groupez):
+        self.groupez = str(groupez)
 
 
 gavestaus.__table__.create(checkfirst=True)
 
 
-def addvar(jasem):
+def addvar(groupez):
     try:
         if SESSION.query(gavestaus).one():
             del_addvar()
     except NoResultFound:
         pass
-    user = gavestaus(jasem)
+    user = gavestaus(groupez)
     SESSION.add(user)
     SESSION.commit()
     SESSION.close()
     return True
 
-
-def del_addvar():
+def del_addvar():   
     to_check = get_addvar()
     if not to_check:
         return False
-    adder = delete(gavestaus)
-    SESSION.execute(adder)
+    abne = delete(gavestaus)
+    SESSION.execute(abne)
     SESSION.commit()
-    return adder
-
+    return abne
 
 def get_addvar():
     try:
-        if _result := SESSION.query(gavestaus).one().jasem:
+        if _result := SESSION.query(gavestaus).one().groupez:
             return _result
     except NoResultFound:
         return None

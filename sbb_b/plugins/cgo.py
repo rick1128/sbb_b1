f5dd65cbd2ebeaa33dbbca683b5aa3abcb7af871
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-10-07 23:15:42 +0300

    Update cgo.py

--- sbb_b/plugins/cgo.py
+++ sbb_b/plugins/cgo.py
@@ -1,4 +1,8 @@
 from ..core.session import sbb_b
-@sbb_b.on(admin_cmd(pattern="فحصص(?: |$)(.*)"))
+@sbb_b.on(admin_cmd(pattern="ليمونة(?: |$)(.*)"))
 async def _(event):    
-    await event.edit("{jmver}")
+    await event.edit("🍋 هاك حطه بطيرك")
+
+@sbb_b.on(admin_cmd(pattern="دي(?: |$)(.*)"))
+async def _(event):    
+    await event.edit("ولي منا فرخ")
